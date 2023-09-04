6dab0baf61272c25660096a37559f26a41ad2c96
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-09-04 19:27:21 +0530

    made navbar responsive for md devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,12 +21,12 @@ const NavbarDefault = () => {
   }, []);
 
   const navList = (
-    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 md:flex-row lg:items-center lg:gap-6">
       <Typography
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-1 font-normal text-[#696c6d] hover:text-[#121212] transition"
+        className="p-1 font-medium text-[#696c6d] hover:text-[#121212] transition"
       >
         <Link to="/" className="flex items-center">
           Home
@@ -36,7 +36,7 @@ const NavbarDefault = () => {
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-1 font-normal text-[#696c6d] hover:text-[#121212] transition"
+        className="p-1 font-medium text-[#696c6d] hover:text-[#121212] transition"
       >
         <Link to="/about" className="flex items-center">
           About
@@ -46,7 +46,7 @@ const NavbarDefault = () => {
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-1 font-normal text-[#696c6d] hover:text-[#121212] transition"
+        className="p-1 font-medium text-[#696c6d] hover:text-[#121212] transition"
       >
         <Link to="/contact" className="flex items-center">
           Contact
@@ -57,58 +57,60 @@ const NavbarDefault = () => {
   );
 
   return (
-    <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
-      <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
-        <Typography
-          as="a"
-          href="/"
-          className="mr-4 cursor-pointer py-1.5 font-medium"
-        >
-          Boku-no-Camera
-        </Typography>
-        <div className="hidden lg:block">{navList}</div>
+    <Navbar className="bg-white">
+      <div className="py-2 px-4 lg:px-8 lg:py-4 mx-auto">
+        <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+          <Typography
+            as="a"
+            href="/"
+            className="mr-4 cursor-pointer py-1.5 font-medium"
+          >
+            Boku-no-Camera
+          </Typography>
+          <div className="hidden md:block">{navList}</div>
 
-        <IconButton
-          variant="text"
-          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-          ripple={false}
-          onClick={() => setOpenNav(!openNav)}
-        >
-          {openNav ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              className="h-6 w-6"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          )}
-        </IconButton>
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent md:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </IconButton>
+        </div>
+        <MobileNav open={openNav}>
+          <div className="container mx-auto">{navList}</div>
+        </MobileNav>
       </div>
-      <MobileNav open={openNav}>
-        <div className="container mx-auto">{navList}</div>
-      </MobileNav>
     </Navbar>
   );
 };
